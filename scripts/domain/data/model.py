c93bceba2abc415c66ff9d7b91f9a31b4270a92d
Toshiya Nakakura <nakakura@me.com>
2020-05-19 19:39:55 +0900

    define Socket and DataSocket

--- scripts/domain/data/model.py
+++ scripts/domain/data/model.py
@@ -2,6 +2,110 @@
 from error import MyException
 
 
+class Socket:
+    def __init__(self, port, ip_v4=u"", ip_v6=u""):
+        """
+        Socket Info. a Pair of address and port
+        :param int port:
+        :param unicode ip_v4:
+        :param unicode ip_v6:
+        """
+
+        if not isinstance(port, int) or port <= 0 or port > 65535:
+            raise MyException("invalid port num")
+        self.__port = port
+
+        if not isinstance(ip_v4, unicode) or not isinstance(ip_v6, unicode):
+            raise MyException("invalid address info")
+
+        if len(ip_v4) == 0 and len(ip_v6) == 0:
+            raise MyException("no address info")
+
+        if len(ip_v4) != 0:
+            self.__is_ip_v4 = True
+        else:
+            self.__is_ip_v4 = False
+        self.__ip_v4 = ip_v4
+        self.__ip_v6 = ip_v6
+
+    def port(self):
+        """
+        Get Port Num
+        :return: port num
+        :rtype: int
+        """
+        return self.__port
+
+    def is_ip_v4(self):
+        """
+        Indicates this object includes IPv4 addr or not
+        :return: True if it includes IPv4 addr
+        :rtype: bool
+        """
+        return self.__is_ip_v4
+
+    def ip_v4(self):
+        """
+        Get IPv4 addr
+        :return: address
+        :rtype: unicode
+        """
+        return self.__ip_v4
+
+    def ip_v6(self):
+        """
+        Get IPv6 addr
+        :return: address
+        :rtype: unicode
+        """
+        return self.__ip_v6
+
+    def __eq__(self, other):
+        if not isinstance(other, Socket):
+            return NotImplemented
+
+        return (
+            self.port() == other.port()
+            and self.is_ip_v4() == other.is_ip_v4()
+            and self.ip_v4() == other.ip_v4()
+            and self.ip_v6() == other.ip_v6()
+        )
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class DataSocket:
+    def __init__(self, data_id, port, ip_v4=u"", ip_v6=u""):
+        self.__data_id = DataId(data_id)
+        self.__socket = Socket(port, ip_v4=ip_v4, ip_v6=ip_v6)
+
+    def socket(self):
+        """
+        Socket Info
+        :return: Socket
+        :rtype: Socket
+        """
+        return self.__socket
+
+    def data_id(self):
+        """
+        Data Id
+        :return: Data Id
+        :rtype: DataId
+        """
+        return self.__data_id
+
+    def __eq__(self, other):
+        if not isinstance(other, DataSocket):
+            return NotImplemented
+
+        return self.data_id() == other.data_id() and self.socket() == other.socket()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
 class DataId:
     def __init__(self, data_id):
         """
@@ -26,9 +130,6 @@ class DataId:
         if not isinstance(other, DataId):
             return NotImplemented
 
-        import rospy
-
-        rospy.logerr("in eq {}, {}", self.id(), other.id())
         return self.id() == other.id()
 
     def __ne__(self, other):
--- test/domain/data/test_data_model.py
+++ test/domain/data/test_data_model.py
@@ -11,12 +11,133 @@ sys.path.append(
     + "/scripts"
 )
 from error import MyException
-from domain.data.model import DataId, DataConnectionId
+from domain.data.model import Socket, DataSocket, DataId, DataConnectionId
 
 PKG = "skyway"
 
 
 class TestDataModel(unittest.TestCase):
+    def test_create_socket_success(self):
+        socket = Socket(10000, ip_v4=u"127.0.0.1")
+        socket2 = Socket(10000, ip_v4=u"127.0.0.1")
+        self.assertEqual(socket.port(), 10000)
+        self.assertEqual(socket.is_ip_v4(), True)
+        self.assertEqual(socket.ip_v4(), u"127.0.0.1")
+        self.assertEqual(socket.ip_v6(), u"")
+        self.assertEqual(socket2.port(), 10000)
+        self.assertEqual(socket2.is_ip_v4(), True)
+        self.assertEqual(socket2.ip_v4(), u"127.0.0.1")
+        self.assertEqual(socket2.ip_v6(), u"")
+        self.assertEqual(socket, socket2)
+
+    def test_create_socket_success_other_port(self):
+        socket = Socket(10000, ip_v4=u"127.0.0.1")
+        socket2 = Socket(10001, ip_v4=u"127.0.0.1")
+        self.assertEqual(socket.port(), 10000)
+        self.assertEqual(socket.is_ip_v4(), True)
+        self.assertEqual(socket.ip_v4(), u"127.0.0.1")
+        self.assertEqual(socket.ip_v6(), u"")
+        self.assertEqual(socket2.port(), 10001)
+        self.assertEqual(socket2.is_ip_v4(), True)
+        self.assertEqual(socket2.ip_v4(), u"127.0.0.1")
+        self.assertEqual(socket2.ip_v6(), u"")
+        self.assertNotEqual(socket, socket2)
+
+    def test_create_socket_success_other_ip(self):
+        socket = Socket(10000, ip_v4=u"127.0.0.1")
+        socket2 = Socket(10000, ip_v4=u"127.0.0.2")
+        self.assertEqual(socket.port(), 10000)
+        self.assertEqual(socket.is_ip_v4(), True)
+        self.assertEqual(socket.ip_v4(), u"127.0.0.1")
+        self.assertEqual(socket.ip_v6(), u"")
+        self.assertEqual(socket2.port(), 10000)
+        self.assertEqual(socket2.is_ip_v4(), True)
+        self.assertEqual(socket2.ip_v4(), u"127.0.0.2")
+        self.assertEqual(socket2.ip_v6(), u"")
+        self.assertNotEqual(socket, socket2)
+
+    def test_create_socket_success_other_stack(self):
+        socket = Socket(65535, ip_v4=u"127.0.0.1")
+        socket2 = Socket(65535, ip_v6=u"fe00::1")
+        self.assertEqual(socket.port(), 65535)
+        self.assertEqual(socket.is_ip_v4(), True)
+        self.assertEqual(socket.ip_v4(), u"127.0.0.1")
+        self.assertEqual(socket.ip_v6(), u"")
+        self.assertEqual(socket2.port(), 65535)
+        self.assertEqual(socket2.is_ip_v4(), False)
+        self.assertEqual(socket2.ip_v4(), u"")
+        self.assertEqual(socket2.ip_v6(), u"fe00::1")
+        self.assertNotEqual(socket, socket2)
+
+    def test_create_socket_fail_due_to_port_num(self):
+        with self.assertRaises(MyException):
+            _socket = Socket(0, ip_v4=u"127.0.0.1")
+
+    def test_create_socket_fail_due_to_port_num_exceed(self):
+        with self.assertRaises(MyException):
+            _socket = Socket(65536, ip_v4=u"127.0.0.1")
+
+    def test_create_socket_fail_invalid_port_type(self):
+        with self.assertRaises(MyException):
+            _socket = Socket("0", ip_v4=u"127.0.0.1")
+
+    def test_create_socket_fail_invalid_ip_v4_type(self):
+        with self.assertRaises(MyException):
+            _socket = Socket(10000, ip_v4="0")
+
+    def test_create_socket_fail_invalid_ip_v6_type(self):
+        with self.assertRaises(MyException):
+            _socket = Socket(10000, ip_v6="0")
+
+    def test_create_socket_fail_invalid_no_ip(self):
+        with self.assertRaises(MyException):
+            _socket = Socket(10000, ip_v4=u"")
+
+    def test_create_data_socket_success(self):
+        data_socket = DataSocket(
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+            10000,
+            ip_v4=u"127.0.0.1",
+            ip_v6=u"",
+        )
+        data_socket2 = DataSocket(
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+            10000,
+            ip_v4=u"127.0.0.1",
+            ip_v6=u"",
+        )
+        self.assertEqual(data_socket, data_socket2)
+
+    def test_create_data_socket_success_other_stack(self):
+        data_socket = DataSocket(
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+            10000,
+            ip_v4=u"127.0.0.1",
+            ip_v6=u"",
+        )
+        data_socket2 = DataSocket(
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+            10000,
+            ip_v4=u"",
+            ip_v6=u"fe00::1",
+        )
+        self.assertNotEqual(data_socket, data_socket2)
+
+    def test_create_data_socket_success_other_port(self):
+        data_socket = DataSocket(
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+            10000,
+            ip_v4=u"127.0.0.1",
+            ip_v6=u"",
+        )
+        data_socket2 = DataSocket(
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+            10001,
+            ip_v4=u"127.0.0.1",
+            ip_v6=u"",
+        )
+        self.assertNotEqual(data_socket, data_socket2)
+
     def test_create_data_id_success(self):
         data_id = DataId(u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211")
         data_id2 = DataId(u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211")
